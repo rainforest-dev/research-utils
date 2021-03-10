68b3fe3d506ad3ea81cce007662ca27d26ab128f
Rainforest Cheng <r08521610@ntu.edu.tw>
2021-03-10 21:28:20 +0800

    add query function to Helper

--- research_utils/sqlite/helper.py
+++ research_utils/sqlite/helper.py
@@ -1,7 +1,8 @@
-from typing import Dict
-from sqlite_utils import Database
+from typing import Dict, List
 from enum import Enum
-
+from functools import reduce
+from sqlite_utils import Database
+from .sql import SQLArgument
 
 class FieldType(Enum):
   INTEGER = "INTEGER"
@@ -16,3 +17,21 @@ class Helper:
     self.__db = Database(db_path)
     self.__table_name = table_name
     self.__fields = fields
+
+    if (not self.__db[self.__table_name].exists() and self.__fields is not None):
+      self.__db[self.__table_name].create(self.__fields, pk=pk)
+      print(f'created table: {self.__table_name}')
+  
+  @property
+  def fields(self):
+    return list(self.__fields.keys())
+  @property
+  def db(self):
+    return self.__db
+
+  def query(self, fields: List[str], argument: SQLArgument=None):
+    select = reduce(lambda field1, field2: f'{field1}, {field2}', fields) if len(fields) > 0 else '*'
+    sql = argument.sql if argument is not None else None
+    print(select, sql)
+    rows = self.__db[self.__table_name].rows_where(select=select)
+    return list(rows)
