396d8ec4ed5312a6040c4f8a24c58930ca178d9d
Rainforest Cheng <r08521610@ntu.edu.tw>
2021-09-18 22:11:32 +0800

    [ADD] add config_logger and logger decorators

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,3 +1,4 @@
+from research_utils.decorators.logging import Logging_Level, config_logger, logger
 from research_utils.decorators.arguments import ignore_unknown_kwargs
 
 
@@ -27,3 +28,18 @@ def test_ignore_unknown_kwargs():
   assert (keyword_with_default(2, 2, False))
   assert (variable_length(2, 3, 5, 6, z=3))
   assert (keyword_only(1, y=3))
+
+
+def test_logging():
+  @config_logger(level=Logging_Level.DEBUG)
+  @logger(level=Logging_Level.WARNING)
+  def log_array():
+    return 1, 2, 3, 4
+
+  @config_logger(level=Logging_Level.INFO)
+  @logger(level=Logging_Level.ERROR, transform=lambda x: f'{x[0]}\t{x[1]}')
+  def log_dict():
+    return {'one': 1, 'two': 2}
+
+  log_array()
+  log_dict()
