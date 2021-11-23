de179e099f3ac2594a10de550bdb9473738fc923
Rainforest Cheng <r08521610@ntu.edu.tw>
2021-11-24 01:33:28 +0800

    [UPDATE] update __repr__ with a cleaner representation

--- research_utils/sqlite/typing/sql.py
+++ research_utils/sqlite/typing/sql.py
@@ -24,6 +24,8 @@ class FieldType(SQLArgument, Enum):
   
   def __str__(self) -> str:
     return self.value
+  def __repr__(self) -> str:
+    return self.value
 
 
 class FieldOption(SQLArgument):
